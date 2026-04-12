9ef1cf7c1b59116c746506ee67886c9842272597
sitsudesh <sudeshkumarmahto112005@gmail.com>
2026-04-12 19:50:54 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter a number");
+        int n= sc.nextInt();
+        for(int i=1;i<=20;i++){
+              System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+    }
